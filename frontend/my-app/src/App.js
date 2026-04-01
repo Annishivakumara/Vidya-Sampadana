b6632f6c85cfbfcd8f96d1f6300318f1c0109635
Anni Shivakumara <ashivu656@gmail.com>
2026-04-01 12:01:59 +0530

    git  error fixed  at about page

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -7,7 +7,6 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
-                <Route path="/about" element={<AboutPage />} />
             </Routes>
         </BrowserRouter>
     );
